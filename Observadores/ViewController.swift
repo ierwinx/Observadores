fc73c31ad58f1988eb181d8d619526dd7f896216
Erwin Luz Leon <eluz@bancoazteca.com>
2021-02-09 10:25:13 -0600

    Initial Commit

---
+++ Observadores/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Observadores
+//
+//  Created by Erwin on 09/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
